cf896feb7429aae792f4866ba8a9b6597bfc9f9c
Tomas Barton <tbarton@ualberta.ca>
2017-10-06 15:41:55 -0600

    Namelist generation for GEOGRID.

--- tests/test_geogrid.py
+++ tests/test_geogrid.py
@@ -3,6 +3,9 @@
 import pytest
 
 from wrf_runner import geogrid, WrfException
+from wrf_runner.namelist import generate_config_file
+from wrf_runner.namelist_geogrid import geogrid_namelist, list_from_list_of_dict, \
+    config_to_namelist
 
 
 @pytest.mark.parametrize("config", [{}, 5, 'nonsense'])
@@ -13,11 +16,11 @@ def test_basic_wrong_configuration(config):
 
 valid_config_1 = {
     "domains": [{
-        "id": 1,
         "parent_id": 1,
         "parent_ratio": 1,
         "parent_start": [1, 1],
-        "size": [74, 61]
+        "size": [74, 61],
+        "step_size": [9000, 9000]
     }],
 
     "projection": {
@@ -38,7 +41,6 @@ def test_valid_configuration(config):
 invalid_configs = [
     {
         "domains": [{
-            "id": 1,
             "parent_ratio": 1,
             "parent_start": [1, 1],
             "size": ["WRONG", 61]
@@ -55,7 +57,6 @@ invalid_configs = [
 
     {
         "domains": [{
-            "id": 1,
             # "parent_id": 1, MISSSING!
             "parent_ratio": 1,
             "parent_start": [1, 1],
@@ -73,7 +74,6 @@ invalid_configs = [
 
     {
         "domains": [{
-            "id": 1,
             "parent_id": 1,
             "parent_ratio": 1,
             "parent_start": [1],  # Only one
@@ -87,7 +87,146 @@ invalid_configs = [
             "stand_lot": -98
         },
         "data_path": "/datasets/GEOGDATA"
-    }
+    },
+
+    {
+        "domains": [{
+            "parent_id": 1,
+            "parent_ratio": 1,
+            "parent_start": [1, 1],
+            "size": [74, 61]
+            # "step_size": [9000, 9000]    Step size missing
+        }],
+
+        "projection": {
+            "type": "lambert",
+            "ref_location": [34.83, -81.03],
+            "truelat": [30, 60],
+            "stand_lot": -98
+        },
+        "data_path": "/datasets/GEOGDATA"
+    },
+
+    {
+        "domains": [
+            {
+                "parent_id": 1,
+                "parent_ratio": 1,
+                "parent_start": [1, 1],
+                "size": [74, 61],
+                "step_size": [9000, 9000]
+            },
+            {
+                "parent_id": 1,
+                "parent_ratio": 1,
+                "parent_start": [1, 1],
+                "size": [74, 61],
+                "step_size": [9000, 9000]  # Step size specified on the second domain
+            }
+        ],
+
+        "projection": {
+            "type": "lambert",
+            "ref_location": [34.83, -81.03],
+            "truelat": [30, 60],
+            "stand_lot": -98
+        },
+        "data_path": "/datasets/GEOGDATA"
+    },
+
+    # First domain wrong parent id
+    {
+        "domains": [
+            {
+                "parent_id": 2,
+                "parent_ratio": 1,
+                "parent_start": [1, 1],
+                "size": [74, 61],
+                "step_size": [9000, 9000]
+            },
+            {
+                "parent_id": 1,
+                "parent_ratio": 1,
+                "parent_start": [1, 1],
+                "size": [74, 61]
+            }
+        ],
+
+        "projection": {
+            "type": "lambert",
+            "ref_location": [34.83, -81.03],
+            "truelat": [30, 60],
+            "stand_lot": -98
+        },
+        "data_path": "/datasets/GEOGDATA"
+    },
+
+    # No domains
+    {
+        "domains": [],
+
+        "projection": {
+            "type": "lambert",
+            "ref_location": [34.83, -81.03],
+            "truelat": [30, 60],
+            "stand_lot": -98
+        },
+        "data_path": "/datasets/GEOGDATA"
+    },
+
+    # Second domain wrong parent id
+    {
+        "domains": [
+            {
+                "parent_id": 1,
+                "parent_ratio": 1,
+                "parent_start": [1, 1],
+                "size": [74, 61],
+                "step_size": [9000, 9000]
+            },
+            {
+                "parent_id": 2,
+                "parent_ratio": 1,
+                "parent_start": [1, 1],
+                "size": [74, 61]
+            }
+        ],
+
+        "projection": {
+            "type": "lambert",
+            "ref_location": [34.83, -81.03],
+            "truelat": [30, 60],
+            "stand_lot": -98
+        },
+        "data_path": "/datasets/GEOGDATA"
+    },
+
+    # Second domain wrong parent id
+    {
+        "domains": [
+            {
+                "parent_id": 1,
+                "parent_ratio": 1,
+                "parent_start": [1, 1],
+                "size": [74, 61],
+                "step_size": [9000, 9000]
+            },
+            {
+                "parent_id": 0,
+                "parent_ratio": 1,
+                "parent_start": [1, 1],
+                "size": [74, 61]
+            }
+        ],
+
+        "projection": {
+            "type": "lambert",
+            "ref_location": [34.83, -81.03],
+            "truelat": [30, 60],
+            "stand_lot": -98
+        },
+        "data_path": "/datasets/GEOGDATA"
+    },
 ]
 
 
@@ -95,3 +234,78 @@ invalid_configs = [
 def test_basic_invalid_configuration(config):
     with pytest.raises(WrfException):
         geogrid.check_config(config)
+
+
+def test_geogrid_namelist_documentation():
+    """
+    Checks if all of the definitions in the structure are copied correctly.
+
+    Only check if the first word of the documentation is equal to the key.
+    :return:
+    """
+
+    for _, section_config in geogrid_namelist.items():
+        for option, config_tuple in section_config.items():
+            first_word = config_tuple[0].split()[0]
+
+            assert (option.lower() == first_word.lower())
+
+
+def test_list_from_list_of_dict():
+    test_list = [{
+        "parent_id": 1,
+        "parent_ratio": 1,
+        "parent_start": [1],  # Only one
+        "size": [74, 61]
+    }, {
+        "parent_id": 1,
+        "parent_ratio": 3,
+        "parent_start": [1],  # Only one
+        "size": [100, 200]
+    }]
+
+    assert (list_from_list_of_dict(test_list, 'parent_ratio') == [1, 3])
+    assert (list_from_list_of_dict(test_list, 'size', selector=lambda x: x[0]) == [74, 100])
+    assert (list_from_list_of_dict(test_list, 'size', selector=lambda x: x[1]) == [61, 200])
+
+
+valid_config_3_domains = {
+    "domains": [
+        {
+            "parent_id": 1,
+            "parent_ratio": 1,
+            "parent_start": [30, 30],
+            "size": [74, 61],
+            "step_size": [9000, 9000]
+        },
+        {
+            "parent_id": 2,
+            "parent_ratio": 3,
+            "parent_start": [30, 30],
+            "size": [100, 120],
+            "step_size": [9000, 9000]
+        },
+        {
+            "parent_id": 3,
+            "parent_ratio": 3,
+            "parent_start": [50, 50],
+            "size": [120, 100]
+        },
+    ],
+
+    "projection": {
+        "type": "lambert",
+        "ref_location": [34.83, -81.03],
+        "truelat": [30, 60],
+        "stand_lot": -98
+    },
+    "data_path": "/datasets/GEOGDATA"
+}
+
+
+def test_config_to_namelist_basic():
+    # Should not fail
+    namelist = config_to_namelist(valid_config_3_domains)
+
+    # Should be accepted by the file generator without any exception
+    generate_config_file(namelist)
--- wrf_runner/geogrid.py
+++ wrf_runner/geogrid.py
@@ -5,6 +5,8 @@ Code to represent and manipulate the GEOGRID program.
 """
 
 import jsonschema
+
+from . import namelist_geogrid
 from .wrf_exceptions import WrfException
 
 two_numbers = {"type": "array", "items": {"type": "number"}, "minItems": 2, "maxItems": 2}
@@ -14,16 +16,17 @@ configuration_schema = {
     "properties": {
         "domains": {
             "type": "array",
+            "minItems": 1,
             "items": {
                 "type": "object",
                 "properties": {
-                    "id": {"type": "number"},
                     "parent_id": {"type": "number"},
                     "parent_ratio": {"type": "number"},
                     "parent_start": two_numbers,
-                    "size": two_numbers
+                    "size": two_numbers,
+                    "step_size": two_numbers
                 },
-                "required": ["id", "parent_id", "parent_ratio", "parent_start", "size"]
+                "required": ["parent_id", "parent_ratio", "parent_start", "size"]
             }},
         "projection": {
             "type": "object",
@@ -40,9 +43,52 @@ configuration_schema = {
     "required": ["domains", "projection", "data_path"]
 }
 
+
 def check_config(config):
+    """
+    Checks if the config file has all that we need to generate the input file for geogrid
+    :param config: a dictionary with the configuration
+    :return: nothing if the config is OK, raises WrfException on error
+    """
     try:
         jsonschema.validate(config, configuration_schema)
 
+        # Check if it has stepsize in the first domain but not the others
+        if 'step_size' not in config['domains'][0]:
+            raise WrfException("Step size not specified in the first domain")
+
+        if any(['step_size' in domain for domain in config['domains'][1:]]):
+            raise WrfException("Step size can be specified only on the first domain")
+
+        # Check first domain id
+        if config['domains'][0]['parent_id'] != 1:
+            raise WrfException('First domain parent id has to be 1.')
+
+        # Check other domains ids
+        for i, domain in enumerate(config['domains'][1:]):
+            if domain['parent_id'] > i:
+                raise WrfException('Invalid parent_id (too large)')
+            if domain['parent_id'] < 1:
+                raise WrfException('Invalid parent_id. Has to be bigger than 1.')
+
     except jsonschema.ValidationError as e:
         raise WrfException("Configuration error", e)
+
+
+class Geogrid:
+    def __init__(self):
+        """"""
+        self.config = None
+
+    def set_config(self, config):
+        check_config(config)
+        self.config = config
+
+    def generate_namelist_dict(self):
+        """
+        Uses the config file to generate the namelist for GEOGRID
+        :return: dictionary with the configuration for the namelist. Can be passed to the Namelist class to merge it
+        with the configuration for the other programs
+        """
+
+        return namelist_geogrid(self.config)
